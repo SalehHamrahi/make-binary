d1ea7f53c59872ab10bdab4c312f45a082638266
Mohammad Saleh Hamrahi <92927665+SalehHamrahi@users.noreply.github.com>
2024-04-21 07:54:15 -0700

    +

--- maker.sh
+++ maker.sh
@@ -1,1 +1,15 @@
-test
+#!/bin/bash
+make
+rm -rf $1
+mkdir $1
+mkdir $1/lib
+cp /usr/local/StarerLibRCSC/lib/* $1/lib -r
+cp /src/* $1/ -r
+cd $1/
+rm *.cpp *.h
+cd ../
+tar -zcvf $1.tar.gz $1/
+rm -rf $1/
+mkdir binary/
+mv $1.tar.gz ./binary/
+mv ./binary ../
