46f29c7bf4ea723b987aba6f1d7889968c26bf75
SalehHamrahi <saleh.hamrahi1386@gmail.com>
2025-05-18 11:17:34 +0330

    Project completion

--- maker.sh
+++ maker.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
-echo ""
+Signature='
+ /$$$$$$$                        /$$           /$$      /$$           /$$                          
+| $$__  $$                      | $$          | $$$    /$$$          | $$                          
+| $$  \ $$ /$$   /$$  /$$$$$$$ /$$$$$$        | $$$$  /$$$$  /$$$$$$ | $$   /$$  /$$$$$$   /$$$$$$ 
+| $$  | $$| $$  | $$ /$$_____/|_  $$_/        | $$ $$/$$ $$ |____  $$| $$  /$$/ /$$__  $$ /$$__  $$
+| $$  | $$| $$  | $$|  $$$$$$   | $$          | $$  $$$| $$  /$$$$$$$| $$$$$$/ | $$$$$$$$| $$  \__/
+| $$  | $$| $$  | $$ \____  $$  | $$ /$$      | $$\  $ | $$ /$$__  $$| $$_  $$ | $$_____/| $$      
+| $$$$$$$/|  $$$$$$/ /$$$$$$$/  |  $$$$/      | $$ \/  | $$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$      
+|_______/  \______/ |_______/    \___/        |__/     |__/ \_______/|__/  \__/ \_______/|__/      
+
+---------------------------------------------------------------------------------------------------
+                                         ***Dust_Maker***
+---------------------------------------------------------------------------------------------------
+by :
+    saleh hamrahi
+    sadra hamrahi
+---------------------------------------------------------------------------------------------------
+'
+echo -e "$Signature"
 echo "team name : "
 read tm
 echo ""
-echo "Do you have two files start and localstartall ? [y/n] "
-read yORn
-make
-rm start localstartAll
+echo "Please wait a few seconds ..."
+echo ""
+make &> a
+rm src/start src/localstartAll &> a
 rm -rf $tm/
 mkdir $tm/
 mkdir $tm/lib
-cp /usr/local/StarerLibRCSC/lib/* $tm/lib -r
+cp /usr/local/StarerLibRCSC/lib/* $tm/lib -r &> a
 cp ./src/* $tm/ -r
 cd $tm/
 rm *.cpp *.h *.o
 cd ../
-if [ $yORn == "n" | $yORn == "N" ] ; then
-    git clone https://github.com/SalehHamrahi/start-localstartAll.git 
-    mv ./start-localstartAll/start ./$tm/
-    mv ./start-localstartAll/localStartAll ./$tm/
-    rm -rf ./start-localstartAll/
-fi
+
+git clone https://github.com/SalehHamrahi/start-localstartAll.git &> a
+mv ./start-localstartAll/start ./$tm/
+mv ./start-localstartAll/localStartAll ./$tm/
+rm -rf ./start-localstartAll/
+
 cd ./$tm/
-sed 's/std-base/'$tm'/g' ./start > ./q
-rm -rf ./start
-mv q start
+sed -i -r 's/std-base/'$tm'/g' start
 cd ..
-tar -zcvf $tm.tar.gz $tm/
+tar -zcf $tm.tar.gz $tm/
 rm -rf $tm/
-mkdir ../binary/
+rm a
+mkdir ../binary/ &> a
+rm a
 mv $tm.tar.gz ../binary/
-mv ./binary ../
+sleep 1
+echo "Binary build completed successfully."
